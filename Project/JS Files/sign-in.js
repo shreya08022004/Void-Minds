3154fd9ddacc15dc8470120bd3b66ebb27da7cdc
shreya08022004 <135721328+shreya08022004@users.noreply.github.com>
2024-03-01 15:15:20 +0530

    Add files via upload

---
+++ Project/JS Files/sign-in.js
@@ -0,0 +1,12 @@
+function signIn(){
+    window.open("/Html-Files/Sign-In-Pages/signin-page.html", "_self");
+}
+function asignIn(){
+    window.open("/Html-Files/Site-Pages/Interviewer-Pages/interviewer-form.html", "_self");
+}
+function usignIn(){
+    window.open("/Html-Files/Site-Pages/Interviewee-Pages/question.html", "_self");
+}
+function irsignIn(){
+    window.open("/Html-Files/Site-Pages/Interviewer-Pages/thanks.html", "_self");
+}
